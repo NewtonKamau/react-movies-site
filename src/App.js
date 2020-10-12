c485b7ee176bb8c9ac1a08d5ffef93d57cca88c7
NewtonKamau <kamaunewton78@gmail.com>
2020-10-12 12:37:20 +0300

    getting image,title and movie rating to the ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Movie from "./components/Movie";
+
+const popularMovies = "https://api.themoviedb.org/3/movie/popular?api_key=7881030c71067049ff65f2cede7914f1&language=en-US&page=1";
+const apiKey="7881030c71067049ff65f2cede7914f1";
+const movieSearch="https://api.themoviedb.org/3/search/movie?api_key=7881030c71067049ff65f2cede7914f1&language=en-US&query="
+   
 
 function App() {
+const [movies,setMovies] = useState([]);
+useEffect(() => {
+ fetch(popularMovies)
+ .then((res)=>res.json())
+ .then((data)=> setMovies(data.results))
+}, []);
   return (
-   <div>Hello Movie lover</div>
+  <div className="movie-container">
+   {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie}/>)}
+   
+  </div>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const movieImages="http://image.tmdb.org/t/p/w1280";
+
+const Movie = ({title,poster_path,overview,vote_average}) => {
+    return (
+        <div >
+       <div className="movie ">
+       <img src={movieImages + poster_path} alt={title} />
+    
+   <div className= "movie-info ">
+     <h3>{title}</h3>
+      
+   <span>Rating {vote_average}</span>
+  </div>
+   <div className="movie-over">
+   <h2>Overview</h2>
+    <p>{overview}</p>
+   </div>
+   </div>
+   </div>
+    )
+}
+
+ 
+export default Movie;
